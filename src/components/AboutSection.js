2409439261279e4cf5bfc955b59461f04733820d
Tony <25971310+MrBanh@users.noreply.github.com>
2020-12-24 21:41:12 -0800

    UPDATE: About section with avatar

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -7,13 +7,32 @@ const AboutSection = () => {
 		section-y
 		section-x
 		">
-			<h2>About Me</h2>
+			<h2 className="mb-8">About Me</h2>
 
 			{/* About Content */}
-			<div>
-				<div>
+			<div className="
+			md:grid
+			md:grid-cols-5
+			md:gap-x-12
+			">
+				<div className="
+				md:col-span-3
+				">
 					<Background />
 					<Skills />
+				</div>
+
+				<div className="
+				flex
+				mt-12
+				mx-auto
+				justify-center
+				items-center
+				max-w-xs
+				md:mt-0
+				md:col-span-2
+				">
+					<img src="/avatar.svg" alt="avatar" />
 				</div>
 			</div>
 		</section>
