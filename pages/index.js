1c8f725b310bbca1d45e0e7f9f798278763ca62b
Tony <25971310+MrBanh@users.noreply.github.com>
2020-12-27 03:38:37 -0800

    NEW: Projects components + images

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,7 @@ import Head from "next/head";
 import Header from "../src/components/Header";
 import IntroSection from "../src/components/IntroSection";
 import AboutSection from "../src/components/AboutSection";
-// import ProjectsSection from "../src/components/ProjectsSection";
-
+import ProjectsSection from "../src/components/ProjectsSection";
 
 export default function Home(){
 	return (
@@ -45,7 +44,7 @@ export default function Home(){
 			">
 				<IntroSection />
 				<AboutSection />
-				{/* <ProjectsSection /> */}
+				<ProjectsSection />
 			</main>
 
 			<footer />
---
+++ src/components/ProjectsSection.js
@@ -0,0 +1,17 @@
+const ProjectsSection = () => {
+	return (
+		<section
+			id="projects"
+			className="
+        section-y
+        section-x
+        "
+		>
+			<h2 className="
+            pb-8
+            ">Projects</h2>
+		</section>
+	);
+};
+
+export default ProjectsSection;
