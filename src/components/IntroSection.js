2646508fab58fdcafac76e72dc591b18c8c96632
Tony <25971310+MrBanh@users.noreply.github.com>
2020-12-28 16:59:16 -0800

    FIX: Fix intro on smaller screens

--- src/components/IntroSection.js
+++ src/components/IntroSection.js
@@ -6,36 +6,26 @@ const IntroSection = () => (
 	flex-col
 	justify-center
 	items-start
-	h-screen
+	min-h-screen
 	select-none
 	section-y
 	section-x
 	">
 		{/* Intro text / icons */}
 
-		<div className="
-			text-6xl
-			font-bold
-			bg-clip-text
-			bg-gradient-to-r from-teal-400 to-blue-500
-			text-transparent
-			sm:text-7xl
-			md:text-8xl
-			lg:text-9xl
-			">
-			<h1 className="
-			leading-snug
-			">Tony Banh</h1>
+		<div>
+			<h1 className="leading-snug">Tony Banh</h1>
 		</div>
 
 		<div className="
-		text-5xl
+		text-4xl
 		font-bold
 		text-gray-400
 		pb-4
-		sm:text-6xl
-		md:text-7xl
-		lg:text-8xl
+		sm:text-5xl
+		md:text-6xl
+		lg:text-7xl
+		xl:text-8xl
 		">
 			<p>I build websites and stuff.</p>
 		</div>
