165a13c6c2c7e021cf6b55f2976cb8705e6c0af5
Tony <25971310+MrBanh@users.noreply.github.com>
2020-12-31 01:24:12 -0800

    Add lazy load attributes to img tags

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -3,7 +3,7 @@ export const Skill = ({ icon, name }) => (
 	flex
 	items-center
 	">
-		<img className="h-4" src={icon} alt={`${name} icon`} />
+		<img className="h-4" src={icon} alt={`${name} icon`} loading="lazy" />
 		<span className="pl-2">{name}</span>
 	</li>
 );
